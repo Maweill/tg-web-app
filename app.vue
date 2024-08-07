36db290f2046d526ef75048dde1002bd352cb6bd
bednyakov <bednyakov99@gmail.com>
2024-08-07 19:27:42 +0300

    feat: add send transaction logic

--- app.vue
+++ app.vue
@@ -1,74 +1,27 @@
 <template>
   <TonConnectUIProvider :options="tonConnectOptions">
-    <div class="container">
-      <h1>Telegram Mini App</h1>
-      <p>Hi!</p>
-      <p v-if="userInfo">Hello, {{ userInfo.first_name }}!</p>
-      <TonConnectButton class-name="ton-connect-button" />
-    </div>
+    <Suspense>
+      <template #default>
+        <AppContent />
+      </template>
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
   </TonConnectUIProvider>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import { TonConnectUIProvider, TonConnectButton } from "@townsquarelabs/ui-vue";
+import { TonConnectUIProvider } from "@townsquarelabs/ui-vue";
 
-const userInfo = ref(null);
 const tonConnectOptions = {
   manifestUrl: "https://maweill.github.io/tg-web-app/tonconnect-manifest.json",
 };
-
-onMounted(() => {
-  if (window.Telegram && window.Telegram.WebApp) {
-    const webApp = window.Telegram.WebApp;
-    webApp.ready();
-    webApp.expand();
-    userInfo.value = webApp.initDataUnsafe.user;
-  }
-});
 </script>
 
 <style>
 body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
-    Arial, sans-serif;
-  margin: 0;
-  padding: 16px;
   color: var(--tg-theme-text-color, #000000);
   background-color: var(--tg-theme-bg-color, #ffffff);
-}
-
-.container {
-  max-width: 390px;
-  margin: 0 auto;
-}
-
-h1 {
-  font-size: 24px;
-  margin-bottom: 16px;
-}
-
-p {
-  font-size: 16px;
-  margin-bottom: 24px;
-}
-
-button {
-  background-color: var(--tg-theme-button-color, #3390ec);
-  color: var(--tg-theme-button-text-color, #ffffff);
-  border: none;
-  border-radius: 8px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-button:active {
-  opacity: 0.8;
-}
-
-.ton-connect-button {
-  margin-top: 20px;
-  margin-bottom: 20px;
 }
 </style>
---
+++ components/AppContent.vue
@@ -0,0 +1,61 @@
+<template>
+  <UContainer class="py-8 flex justify-center">
+    <UCard>
+      <template #header>
+        <h1 class="text-2xl font-bold">Telegram Mini App</h1>
+      </template>
+      <div class="flex justify-center">
+        <TonConnectButton />
+      </div>
+      <template #footer>
+        <p v-if="userInfo">Hello, {{ userInfo.first_name }}!</p>
+        <div v-else-if="wallet" class="flex flex-col items-center">
+          <p>Address: {{ tonAddress }}</p>
+          <p>Chain: {{ wallet.account.chain }}</p>
+          <p>Provider: {{ wallet.provider }}</p>
+          <p>Device: {{ wallet.device.platform }}</p>
+          <UButton @click="sendTransaction"> Send Transaction </UButton>
+        </div>
+        <p v-else>Please connect your wallet</p>
+      </template>
+    </UCard>
+  </UContainer>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import {
+  TonConnectButton,
+  useTonWallet,
+  useTonAddress,
+  useTonConnectUI,
+} from "@townsquarelabs/ui-vue";
+
+const transaction = {
+  messages: [
+    {
+      address:
+        "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
+      amount: "20000", //Toncoin in nanotons
+    },
+  ],
+};
+
+const userInfo = ref(null);
+const wallet = useTonWallet();
+const tonAddress = useTonAddress();
+const connector = useTonConnectUI();
+
+onMounted(() => {
+  if (window.Telegram && window.Telegram.WebApp) {
+    const webApp = window.Telegram.WebApp;
+    webApp.ready();
+    webApp.expand();
+    userInfo.value = webApp.initDataUnsafe.user;
+  }
+});
+
+function sendTransaction() {
+  connector[0].sendTransaction(transaction);
+}
+</script>
